7979a3032a013e16ebf4d0946dc6a74c9e354d28
liyixin123 <592945148@qq.com>
2020-06-04 14:38:14 +0800

    commit stage 2

--- Tic-Tac-Toe/task/src/tictactoe/Main.java
+++ Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -1,7 +1,20 @@
 package tictactoe;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        System.out.println("Enter cells: ");
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.next();
+        System.out.println("---------");
+        for (int i = 0; i < 3; i++) {
+            System.out.print("| ");
+            for (int j = 0; j < 3; j++) {
+                System.out.print(str.charAt(3 * i + j) + " ");
+            }
+            System.out.println("|");
+        }
+        System.out.println("---------");
     }
 }
